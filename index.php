09963a88e9d2c618170ff34b5fd06658de1e261a
Dylan-Morrissey <dylamorrissey2010@gmail.com>
2022-02-03 21:25:32 +0000

    - Used bootstrap to make table look nicer. - Added Action colum with butons to delete, update, view. - Create view.php to load the selected user.

--- index.php
+++ index.php
@@ -7,33 +7,43 @@ $users = getUsers();
   <head>
     <meta charset="utf-8">
     <title>CRUD app</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="master.css">
   </head>
   <header>
 
   </header>
 
   <body>
-    <table>
+    <table class="table table-dark">
       <thead>
-        <th>Name </th>
-        <th>Username </th>
-        <th>Email address </th>
-        <th>Phone Number </th>
-        <th>website </th>
+        <tr>
+          <th scope="col"> Name </th>
+          <th scope="col">Username </th>
+          <th scope="col">Actions</th>
+          <th scope="col">Email address </th>
+          <th scope="col">Phone Number </th>
+          <th scope="col">Website</th>
+          <th scope="col">Actions</th>
+      </tr>
       </thead>
       <tbody>
         <?php foreach ($users as $user): ?>
         <tr>
-          <td><?php echo $user["name"]; ?></td>
+          <td><?php echo $user['name']; ?></td>
           <td><?php echo $user['username']; ?></td>
           <td><?php echo $user['email']; ?></td>
           <td><?php echo $user['phone']; ?></td>
           <td><?php echo $user['website']; ?></td>
+          <td>
+            <a href="view.php?id=<?php echo $user['id'] ?>" class="btn btn-info">View</a>
+            <a href="update.php=<?php echo $user['id'] ?>" class="btn btn-secondary">Update</a>
+            <a href="delete.php=<?php echo $user['id'] ?>" class="btn btn-danger">Delete</a>
+          </td>
         </tr>
       <?php endforeach; ?>
       </tbody>
     </table>
-
   </body>
 
   <footer>
---
+++ view.php
@@ -0,0 +1,53 @@
+<?php
+
+require 'users.php';
+
+
+if (!isset($_GET['id'])) {
+  echo "not found";
+  exit;
+}
+
+$userId = $_GET['id'];
+$user = getUserById($userId);
+
+if (!$user) {
+  echo "Not Found";
+  exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <h3 class="table"><?php echo $user['name']?></h3>
+    <table class="table">
+      <tbody>
+      <tr>
+        <th>Email:</th>
+        <td><?php echo $user['email']; ?></td>
+      </tr>
+      <tr>
+        <th>Phone:</th>
+        <td><?php echo $user['phone']; ?></td>
+      </tr>
+      <tr>
+        <th>Website:</th>
+        <td><?php echo $user['website']; ?></td>
+      </tr>
+      <tr>
+        <th>Username:</th>
+        <td><?php echo $user['username']; ?></td>
+      </tr>
+      </tbody>
+
+    </table>
+  </body>
+</html>
