845dac0e1183c33d40381f965008cec718e7d3db
Dylan-Morrissey <dylamorrissey2010@gmail.com>
2022-02-03 20:00:54 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CRUD app</title>
+  </head>
+  <header>
+
+  </header>
+
+  <body>
+
+  </body>
+
+  <footer>
+
+  </footer>
+
+</html>
