a80c88ffb2516acdfbd86f41f028c63c34e714f5
Dylan-Morrissey <dylamorrissey2010@gmail.com>
2022-02-05 12:33:08 +0000

    Added container to create.php

--- create.php
+++ create.php
@@ -15,34 +15,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
   <body>
+    <div class="container">
+      <h3 class="table">Create new user</h3>
+      <form action="" method="POST" enctype="multipart/form-data">
+        <table class="table">
+          <tbody>
+            <tr>
+              <th><label for="name">Name: </label></th>
+              <td><input name="name"><br><br></td>
+            </tr>
+            <tr>
+              <th><label for="name">Email Address: </label></th>
+              <td><input name="email"><br><br></td>
+            </tr>
+            <tr>
+              <th><label for="name">Phone: </label></th>
+              <td>  <input name="phone" ><br><br></td>
+            </tr>
+            <tr>
+              <th><label for="name">Website: </label></th>
+              <td>  <input name="website" ><br><br></td>
+            </tr>
+            <tr>
+              <th><label for="name">Username: </label></th>
+              <td>  <input name="username" ><br><br></td>
+            </tr>
+          </tbody>
+        </table>
+        <button class="btn btn-success">Save</button>
+      </form>
+    </div>
 
-    <h3 class="table">Create new user</h3>
-    <form action="" method="POST" enctype="multipart/form-data">
-      <table class="table">
-        <tbody>
-          <tr>
-            <th><label for="name">Name: </label></th>
-            <td><input name="name"><br><br></td>
-          </tr>
-          <tr>
-            <th><label for="name">Email Address: </label></th>
-            <td><input name="email"><br><br></td>
-          </tr>
-          <tr>
-            <th><label for="name">Phone: </label></th>
-            <td>  <input name="phone" ><br><br></td>
-          </tr>
-          <tr>
-            <th><label for="name">Website: </label></th>
-            <td>  <input name="website" ><br><br></td>
-          </tr>
-          <tr>
-            <th><label for="name">Username: </label></th>
-            <td>  <input name="username" ><br><br></td>
-          </tr>
-        </tbody>
-      </table>
-      <button class="btn btn-success">Save</button>
-    </form>
   </body>
 </html>
